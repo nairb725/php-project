745fe1fc48bad6a4feaba4fa0cf81e1f39ccbabd
killian <killian.gay28@gmail.com>
2021-02-04 17:21:03 +0100

    add fonction and clean workSpace

---
+++ logIn.php
@@ -0,0 +1,32 @@
+<?php 
+$user = 'groupe5';
+$pass = 'aGx039r^';
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+try {
+    $dbh = new PDO('mysql:host=2eurhost.com;dbname=eurh_groupe5', $user, $pass);
+    $userdb = $dbh->query('SELECT username, password FROM Utilisateur');
+    $data = $userdb->fetchAll();
+    
+    for ($i = 0; $i < sizeof($data); $i++) {
+        
+        if ($data[$i]["username"] == $username && $data[$i]["password"] == $password) {
+            $dbh = null;
+            $userdb = null;
+            header('Location: home.html');
+            exit(); 
+
+        } else if ($i == sizeof($data) - 1) {
+            header('Location: logIn.html');
+            exit();
+        }
+    } 
+
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();  
+}
+
+?>
+
---
+++ signIn.php
@@ -0,0 +1,38 @@
+<?php 
+$user = 'groupe5';
+$pass = 'aGx039r^';
+$usernameInput = $_POST["username"];
+$passwordInput = $_POST["password"];
+
+try {
+    $dbh = new PDO('mysql:host=2eurhost.com;dbname=eurh_groupe5', $user, $pass);
+    $userdb = $dbh->query('SELECT username, password FROM Utilisateur');
+    $data = $userdb->fetchAll();
+    
+    for ($i = 0; $i < sizeof($data); $i++) {
+        
+        if ($data[$i]["username"] == $usernameInput) {
+            $usernameInput = null;
+            $passwordInput = null;
+            header('Location: signIn.html');
+            exit();
+
+        } else if ($i == sizeof($data) - 1) {
+            $stmt = $dbh->prepare ("INSERT INTO Utilisateur (username, password) VALUES (:name, :value)");
+            $stmt->bindParam(':name', $usernameInput);
+            $stmt->bindParam(':value', $passwordInput);
+            $stmt->execute();
+            $usernameInput = null;
+            $passwordInput = null;
+            header('Location: home.html');
+            exit();
+        }        
+    } 
+
+} catch (PDOException $e) {
+    //print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
+
