17db2d0b37939c2123e15067bc4c3fa0c33f78f2
killian <killian.gay28@gmail.com>
2021-02-04 19:16:44 +0100

    delete home file

--- achat.php
+++ achat.php
@@ -27,20 +27,20 @@ try {
 <h1>E-SHOP</h1> 
 
 
-<img src="css/images/renard.webp" alt="image" /><strong>PRIX DE LA PELUCHE RENARD : 25E</strong> &nbsp&nbsp <button class="favorite styled"
+<img src="images/renard.webp" alt="image" /><strong>PRIX DE LA PELUCHE RENARD : 25E</strong> &nbsp&nbsp <button class="favorite styled"
   type="button"> Add to caddy </button> </br>
 
 
-<img src="css/images/shiba.webp" alt="image" /><strong>PRIX DE LA PELUCHE SHIBA : 20E</strong> &nbsp&nbsp <button class="favorite styled"
+<img src="images/shiba.webp" alt="image" /><strong>PRIX DE LA PELUCHE SHIBA : 20E</strong> &nbsp&nbsp <button class="favorite styled"
         type="button"> Add to caddy </button> </br>
 
-<img src="css/images/sushi.webp" alt="image" /><strong>PRIX DE LA PELUCHE SUSHI : 30E</strong> &nbsp&nbsp <button class="favorite styled"
+<img src="images/sushi.webp" alt="image" /><strong>PRIX DE LA PELUCHE SUSHI : 30E</strong> &nbsp&nbsp <button class="favorite styled"
         type="button"> Add to caddy </button> </br>
 
-<img src="css/images/tortue.webp" alt="image" /><strong>PRIX DE LA PELUCHE TORTUE : 15E</strong> &nbsp&nbsp <button class="favorite styled"
+<img src="images/tortue.webp" alt="image" /><strong>PRIX DE LA PELUCHE TORTUE : 15E</strong> &nbsp&nbsp <button class="favorite styled"
         type="button"> Add to caddy </button>  </br>
 
-<img src="css/images/cheshire.jpg" alt="image" /><strong>PRIX DE LA PELUCHE CHESHIRE : 35E</strong> &nbsp&nbsp <button class="favorite styled"
+<img src="images/cheshire.jpg" alt="image" /><strong>PRIX DE LA PELUCHE CHESHIRE : 35E</strong> &nbsp&nbsp <button class="favorite styled"
         type="button"> Add to caddy </button>  </br>
 
  </body>
