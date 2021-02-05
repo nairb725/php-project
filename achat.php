376e2e48d89e67766bb9ed8da7a6af365967773b
Brian <58400498+nairb725@users.noreply.github.com>
2021-02-05 12:11:26 +0100

    Update achat.php

--- achat.php
+++ achat.php
@@ -5,11 +5,21 @@ $pass = 'aGx039r^';
 $host = "2eurhost.com";
 $dbName = 'eurh_groupe5';
 $dataBase = new dataBasePDO($user, $pass, $host, $dbName);
-$fetch2 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 2");
-$fetch1 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 1");
-$fetch3 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 3");
-$fetch4 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 4");
-$fetch5 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 5");
+
+//nomProd
+$fetch2 = $dataBase -> request("SELECT nomProd, Prix, categorie FROM Produit WHERE Nprod = 2");
+$fetch1 = $dataBase -> request("SELECT nomProd, Prix, categorie FROM Produit WHERE Nprod = 1");
+$fetch3 = $dataBase -> request("SELECT nomProd, Prix, categorie FROM Produit WHERE Nprod = 3");
+$fetch4 = $dataBase -> request("SELECT nomProd, Prix, categorie FROM Produit WHERE Nprod = 4");
+$fetch5 = $dataBase -> request("SELECT nomProd, Prix, categorie FROM Produit WHERE Nprod = 5");
+
+
+
+
+
+
+
+
 
 // try {
 //     $dbh = new PDO('mysql:host=2eurhost.com;dbname=eurh_groupe5', $user, $pass);
@@ -29,28 +39,29 @@ $fetch5 = $dataBase -> request("SELECT nomProd, Prix FROM Produit WHERE Nprod = 
 <meta hcharset=utf-8>
 <link rel="stylesheet" href="achat.css" type="text/css" media="all" />
 <link rel="stylesheet" href="bouton.css" type="text/css" media="all" />
-</head>
+
 <h2><button class="favorite styled" href="logIn.php" type="button" onclick="window.location.href = 'logIn.html';"> CONNEXION/INSCRIPTION </button></h2>
+</head>
 <body>
 <h1>E-SHOP</h1> 
 
 
-<img src="/image/renard.webp" alt="image" /><strong> <?php echo("PELUCHE") ?> <?php echo $fetch2[0][ "nomProd"]?> <?php echo("PRIX:") ?> <?php echo $fetch2[0]["Prix"]; ?> <?php echo("E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
+<img src="/image/renard.webp" alt="image" /><strong> <?php echo $fetch2[0][ "categorie"]. (" "). $fetch2[0][ "nomProd"]. (" PRIX: "). $fetch2[0]["Prix"]. (" E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
 type="button"> Add to caddy </button> </br>
 
 
-<img src="/image/shiba.webp" alt="image" /><strong> <?php echo("PELUCHE") ?> <?php echo $fetch1[0][ "nomProd"]?> <?php echo("PRIX:") ?> <?php echo $fetch1[0]["Prix"]; ?> <?php echo("E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
+<img src="/image/shiba.webp" alt="image" /><strong> <?php echo $fetch1[0][ "categorie"]. (" "). $fetch1[0][ "nomProd"]. (" PRIX: "). $fetch1[0]["Prix"]. (" E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
 type="button"> Add to caddy </button> </br>
 
-<img src="/image/sushi.webp" alt="image" /><strong> <?php echo("PELUCHE") ?> <?php echo $fetch3[0][ "nomProd"]?> <?php echo("PRIX:") ?> <?php echo $fetch3[0]["Prix"]; ?> <?php echo("E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
-type="button"> Add to caddy </button> </br>
-
-
-<img src="/image/tortue.webp" alt="image" /><strong> <?php echo("PELUCHE") ?> <?php echo $fetch4[0][ "nomProd"]?> <?php echo("PRIX:") ?> <?php echo $fetch4[0]["Prix"]; ?> <?php echo("E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
+<img src="/image/sushi.webp" alt="image" /><strong> <?php echo $fetch3[0][ "categorie"]. (" ").$fetch3[0][ "nomProd"]. (" PRIX: ").  $fetch3[0]["Prix"]. (" E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
 type="button"> Add to caddy </button> </br>
 
 
-<img src="/image/cheshire.jpg" alt="image" /><strong> <?php echo("SERVIETTE") ?> <?php echo $fetch5[0][ "nomProd"]?> <?php echo("PRIX:") ?> <?php echo $fetch5[0]["Prix"]; ?> <?php echo("E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
+<img src="/image/tortue.webp" alt="image" /><strong> <?php echo $fetch4[0][ "categorie"]. (" ").$fetch4[0][ "nomProd"]. (" PRIX: "). $fetch4[0]["Prix"]. (" E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
+type="button"> Add to caddy </button> </br>
+
+
+<img src="/image/cheshire.jpg" alt="image" /><strong> <?php echo $fetch5[0][ "categorie"]. (" ").$fetch5[0][ "nomProd"]. (" PRIX: "). $fetch5[0]["Prix"]. (" E") ?> </strong> &nbsp&nbsp <button class="favorite styled"
 type="button"> Add to caddy </button> </br>
 
 
